04201e377af104e20e2ed6b4b53bdd6ae4602c04
b110user02 <b1102ueser02@rpsconsulting.in>
2024-07-31 20:35:06 +0530

    spring mvc

---
+++ j2ee/BeanScope/src/DependencyInjection_constructor/Employee.java
@@ -0,0 +1,51 @@
+package DependencyInjection_constructor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import DependencyInjection_setter.Department;
+
+public class Employee {
+    private int empId;
+    private String name;
+    private String dept;
+    private Department department;
+
+    public int getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(int empId) {
+        this.empId = empId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDept() {
+        return dept;
+    }
+
+    public void setDept(String dept) {
+        this.dept = dept;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+    @Autowired
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    public void displayEmployeeInfo() {
+        System.out.println("Employee id " + empId);
+        System.out.println("Name : " + name);
+        System.out.println("Dept type : " + dept);
+        department.displayDepartment(dept);
+    }
+}
---
+++ j2ee/BeanScope/src/DependencyInjection_setter/Department.java
@@ -0,0 +1,15 @@
+package DependencyInjection_setter;
+
+public class Department {
+    public void displayDepartment(String empType) {
+        if (empType.equals("HR")) {
+            System.out.println("HR Department, Role: Recruitment, Employee Benefits");
+        } else if (empType.equals("Admin")) {
+            System.out.println("Admin dept: Transportation to employees, food coupons etc.");
+        } else if (empType.equals("Technology")) {
+            System.out.println("Technology dept: Research and development, training etc.");
+        } else {
+            System.out.println("Unknown Department");
+        }
+    }
+}
